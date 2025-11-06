603be7430182d6f451a8f1d2c3a0c922d5d9ed47
guifre1714 <marcelpm2018@gmail.com>
2025-11-06 16:41:38 +0100

    flippers creats

--- Source/ModuleGame.cpp
+++ Source/ModuleGame.cpp
@@ -71,22 +71,13 @@ update_status ModuleGame::Update()
 {
 	DrawTexture(mapa, 0, 105, WHITE);
 
-	/*Rectangle flipper = {200,200};
-	Vector2 fliPos = { 0,105 };
-	DrawRectanglePro(flipper,fliPos , 5.0, RED);*/
-
-	if (IsKeyPressed(KEY_ONE)) {
-
-		bodies.push_back(App->physics->CreateCircle(GetMouseX(), GetMouseY(), 25));
-	}
-
-	for each (PhysBody * body in bodies)
+	/*for each (PhysBody * body in bodies)
 	{
 		int x, y;
 		body->GetPosition(x, y);
 
 		App->renderer->Draw(circle, x - circle.width * 0.5f, y - circle.height * 0.5f);
-	}
+	}*/
 
 	return UPDATE_CONTINUE;
 }
